eb556a89448510b7f101a4a9dda81ba1475353d1
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2022-07-02 16:16:30 -0300

    checkpoint

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,9 @@
-import { Flex, Icon, Input, Text } from "@chakra-ui/react";
-import {RiSearchLine} from 'react-icons/ri'
+import { Avatar, Box, Flex, HStack, Icon, Input, Stack, Text } from "@chakra-ui/react";
+import {
+  RiNotificationLine,
+  RiSearchLine,
+  RiUserAddLine,
+} from "react-icons/ri";
 
 export function Header() {
   return (
@@ -13,13 +17,14 @@ export function Header() {
       px="6"
       align="center"
     >
+      {/* logo */}
       <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
         DashGo
         <Text as="span" ml="1" color={"pink.500"}>
           .
         </Text>
       </Text>
-
+      {/* Search Bar */}
       <Flex
         as="label"
         flex="1"
@@ -32,15 +37,40 @@ export function Header() {
         bg="gray.800"
         borderRadius="full"
       >
-          <Input
+        <Input
           color="gray.50"
           variant="unstyled"
           px="4"
           mr="4"
           placeholder="Buscar na plataforma"
           _placeholder={{ color: "gray.400" }}
-          />
-          <Icon as={RiSearchLine} fontSize={20} />
+        />
+        <Icon as={RiSearchLine} fontSize={20} />
+      </Flex>
+
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize={20} />
+          <Icon as={RiUserAddLine} fontSize={20} />
+        </HStack>
+
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Luca A. Rampinelli</Text>
+            <Text color="gray.300" fontSize="small">
+              luca.alfaro.rampinelli@gmail.com
+            </Text>
+          </Box>
+          <Avatar size="md" name="Luca Alfaro Rampinelli"/>
+        </Flex>
       </Flex>
     </Flex>
   );
